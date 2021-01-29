b402eea655b70e79a93a50d66e5d94f0b02b87df
Jonathan Samson <jonathansamsonvcu@gmail.com>
2021-01-29 12:53:24 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use std::process::Command;
+use std::process;
+use std::env;
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("missing argument");
+        process::exit(1);
+    }
+
+    args.remove(0);
+    args.remove(0);
+
+    let status = Command::new("cargo")
+             .arg("clean")
+             .spawn().expect("clean failed to execute")
+             .wait().expect("error while waiting for process");
+
+    match status.code() {
+        Some(0) => {},
+        Some(code) => {
+            eprintln!("`cargo clean` exited with status code {}", code);
+            process::exit(code);
+        },
+        None => {
+            eprintln!("`cargo clean` terminated by signal");
+            process::exit(0);
+        }
+    }
+
+    let status = Command::new("cargo")
+             .arg("build")
+             .args(&args)
+             .spawn().expect("process failed to execute")
+             .wait().expect("error while waiting for process");
+
+    let mut command = vec![
+        String::from("cargo"),
+        String::from("build")
+    ];
+    command.extend(args);
+
+    let command = command.join(" ");
+
+    match status.code() {
+        Some(0) => {},
+        Some(code) => {
+            eprintln!("`{}` exited with status code {}", command, code);
+            process::exit(code);
+        },
+        None => {
+            eprintln!("`{}` terminated by signal", command);
+            process::exit(0);
+        }
+    }
+
+}
+
