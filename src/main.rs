e56eec5917cd9a32f41a0a6b1e7e4ab3bfae7aa8
Jonathan Samson <jonathansamsonvcu@gmail.com>
2021-01-29 13:58:34 -0500

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
-use std::process;
 use std::env;
+use std::process;
+use std::process::Command;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -14,16 +14,18 @@ fn main() {
     args.remove(0);
 
     let status = Command::new("cargo")
-             .arg("clean")
-             .spawn().expect("clean failed to execute")
-             .wait().expect("error while waiting for process");
+        .arg("clean")
+        .spawn()
+        .expect("clean failed to execute")
+        .wait()
+        .expect("error while waiting for process");
 
     match status.code() {
-        Some(0) => {},
+        Some(0) => {}
         Some(code) => {
             eprintln!("`cargo clean` exited with status code {}", code);
             process::exit(code);
-        },
+        }
         None => {
             eprintln!("`cargo clean` terminated by signal");
             process::exit(0);
@@ -31,30 +33,27 @@ fn main() {
     }
 
     let status = Command::new("cargo")
-             .arg("build")
-             .args(&args)
-             .spawn().expect("process failed to execute")
-             .wait().expect("error while waiting for process");
+        .arg("build")
+        .args(&args)
+        .spawn()
+        .expect("process failed to execute")
+        .wait()
+        .expect("error while waiting for process");
 
-    let mut command = vec![
-        String::from("cargo"),
-        String::from("build")
-    ];
+    let mut command = vec![String::from("cargo"), String::from("build")];
     command.extend(args);
 
     let command = command.join(" ");
 
     match status.code() {
-        Some(0) => {},
+        Some(0) => {}
         Some(code) => {
             eprintln!("`{}` exited with status code {}", command, code);
             process::exit(code);
-        },
+        }
         None => {
             eprintln!("`{}` terminated by signal", command);
             process::exit(0);
         }
     }
-
 }
-
